3e404f2e50cc6cd4e27110edfd7e0853ecaa3299
Jan Baer <janthebaer@gmail.com>
2023-09-04 13:26:51 +0200

    update README, add setup script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check if python3 and pip3 are installed
+if ! command -v python3 &> /dev/null
+then
+    echo "python3 is not installed. Please install it first."
+    exit 1
+fi
+
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 is not installed. Please install it first."
+    exit 1
+fi
+
+VENV_NAME="nlb-venv"
+
+python3 -m venv $VENV_NAME
+
+source $VENV_NAME/bin/activate
+
+# Check if requirements.txt exists
+if [ ! -f "requirements.txt" ]; then
+    echo "requirements.txt does not exist in the current directory."
+    deactivate
+    exit 1
+fi
+
+# Install packages from requirements.txt
+pip install -r requirements.txt
